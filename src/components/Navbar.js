20cee9b08bf020df57e1000b77a1e831fbab26f3
Tony Shifflett <charles.l.shifflett@gmail.com>
2022-03-01 17:58:54 -0600

    changed react fragment parent element in Navbar.js for div with appropriate className, added styling to NavBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,22 @@
 import React from 'react'
+import '../styles/Navbar.css'
 
 const Navbar = () => {
   return (
-      <>
+      <div className='navbar'>
         <ul className='nav-list'>
-            <li>Restaurant</li>
+            <div className='nav-logo'>
+                <li>Restaurant</li>
+            </div>
+            <div className='nav-menu-items'>
             <li>About</li>
             <li>Gallery</li>
             <li>Testimonial</li>
             <li>1800-123-4567</li>
             <li>RESERVATION</li>
+            </div>
         </ul>
-      </>
+      </div>
   )
 }
 
