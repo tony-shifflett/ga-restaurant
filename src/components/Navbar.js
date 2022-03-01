3980e7bd945f449b77d485ce08f185c1e2c50585
Tony Shifflett <charles.l.shifflett@gmail.com>
2022-03-01 17:40:53 -0600

    created navbar skeleton, added navbar to page stubs, added border to all elements for dev purposes

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+      <>
+        <ul className='nav-list'>
+            <li>Restaurant</li>
+            <li>About</li>
+            <li>Gallery</li>
+            <li>Testimonial</li>
+            <li>1800-123-4567</li>
+            <li>RESERVATION</li>
+        </ul>
+      </>
+  )
+}
+
+export default Navbar
--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 const Gallery = () => {
   return (
-    <div>Gallery</div>
+    <>
+        <Navbar/>
+    </>
   )
 }
 
