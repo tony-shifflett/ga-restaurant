c0f8ec965bf4ef63ab6a5df594fb2223047ca3b1
Tony Shifflett <charles.l.shifflett@gmail.com>
2022-03-02 17:28:42 -0600

    Footer component and style, removed icon from App.js, changed title in index.html

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import './styles/App.css';
-// import {Routes, Route} from 'react-router-dom';
 import Landing from './pages/Landing';
 import About from './pages/About';
 import Gallery from './pages/Gallery';
 import Testimonial from './pages/Testimonial';
+import Footer from './pages/Footer';
 
 function App() {
   return (
     <div className="App">
-      {/* <Routes>
-        <Route exact path="/" element={<Landing/>}/>
-        <Route exact path="/about" element={<About/>}/>
-        <Route exact path="/gallery" element ={<Gallery/>}/>
-        <Route exact path="testimonial" element={<Testimonial/>}/>
-      </Routes> */}
       <Landing/>
       <About/>
       <Gallery/>
       <Testimonial/>
+      <Footer/>
     </div>
   );
 }
