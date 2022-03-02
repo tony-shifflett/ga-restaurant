9925c7cb5aa0ad2c39a87b5eccd59c258a4aee53
Tony Shifflett <charles.l.shifflett@gmail.com>
2022-03-02 16:50:17 -0600

    further styling in gallery, added fontawesome dependencies and icon in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import '../styles/Navbar.css'
-// import {Link} from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPhoneFlip } from '@fortawesome/free-solid-svg-icons'
+
 
 const Navbar = () => {
   return (
@@ -10,15 +12,10 @@ const Navbar = () => {
                 <li>Restaurant</li>
             </div>
             <div className='nav-menu-items'>
-            {/* <li><Link className="name-menu-item" to="/about">About</Link></li>
-            <li><Link className="name-menu-item" to="/gallery">Gallery</Link></li>
-            <li><Link className="name-menu-item" to="/testimonial">Testimonial</Link></li>
-            <li>1800-123-4567</li>
-            <li><button type="button">RESERVATION</button></li> */}
             <li><a className="name-menu-item" href="#about-page">About</a></li>
             <li><a className="name-menu-item" href="#gallery">Gallery</a></li>
             <li><a className="name-menu-item" href="#reviews">Testimonial</a></li>
-            <li>1800-123-4567</li>
+            <li><FontAwesomeIcon icon={faPhoneFlip} />1800-123-4567</li>
             <li><button type="button">RESERVATION</button></li>
             </div>
         </ul>
