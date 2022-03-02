8801ee121c8f5b7b32e90f55da2c344f0f5a08d5
Tony Shifflett <charles.l.shifflett@gmail.com>
2022-03-01 18:08:52 -0600

    added styling to landing page, modified navbar styling

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import '../styles/Landing.css'
 
 const Landing = () => {
   return (
-    <>
+    <div className='landing-page'>
         <Navbar/>
-    </>
+    </div>
   )
 }
 
