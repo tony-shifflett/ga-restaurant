d5a50ca5505f18d4970bf05c262e74d84e78b210
Tony Shifflett <charles.l.shifflett@gmail.com>
2022-03-04 11:13:05 -0600

    removed unused import in Footer

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import '../styles/Footer.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {FaFacebookSquare, FaTwitterSquare, FaBehance, FaPinterestSquare, FaLinkedin} from 'react-icons/fa'
 const Footer = () => {
   return (
