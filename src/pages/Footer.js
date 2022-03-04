cd4379bbea2f308b91c40b56ba1096be5b322a83
Tony Shifflett <charles.l.shifflett@gmail.com>
2022-03-03 20:30:09 -0600

    added content and styling to footer

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import '../styles/Footer.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FaFacebookSqu } from '@fortawesome/free-solid-svg-icons'
 
 const Footer = () => {
   return (
     <footer id='footer'>
-        Footer
+        <div id='copyright'>
+          2022 Copyrights. All rights reserved.
+        </div>
+        <div id='social-media'>
+          {/* placeholder text */}
+          Social Media Icons
+        </div>
     </footer>
   )
 }
